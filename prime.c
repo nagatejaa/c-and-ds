7c2723d6a7ec393e7c68cbf8546c18b82b525c42
nagatejaa <141476998+nagatejaa@users.noreply.github.com>
2023-10-10 00:04:34 +0530

    Update prime.c

--- prime.c
+++ prime.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 int main(){
     int num,i,j,flag = 0;
-    i = 1;
+    // i = 2;
     scanf("%d",&num);
+    i = 2;
     while(i<=num){
         j = 2;
         while(j<i){
-            if(i % j == 0){
+            if(i%j==0){
                 flag++;
+                break;
             }
             j++;
+        } 
+        if(flag==0){
+            printf("%d\n",i);
         }
-        if(flag==2){
-            printf("%d ",i);
-        }
+        flag=0;
         i++;
     }
     return 0;
